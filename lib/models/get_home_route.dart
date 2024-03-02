c1e81bdcdac044099283b9583c6a173a77207ec6
finnik <niklas@finnik.de>
2024-03-02 18:05:52 +0100

    Basic route model
    - try to make this project multi-user friendly

---
+++ lib/models/get_home_route.dart
@@ -0,0 +1,7 @@
+class GetHomeRoute {
+  // TODO Declare the attributes necessary to display the route correctly
+  final DateTime departureTime;
+  final String line;
+
+  GetHomeRoute({required this.departureTime, required this.line});
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,8 @@
+import 'package:gethome/models/get_home_route.dart';
+
+class GoogleAPI {
+  static List<GetHomeRoute> getRoutes() {
+    // TODO Make the API calls and return a list of possible routes
+    return List.empty();
+  }
+}
