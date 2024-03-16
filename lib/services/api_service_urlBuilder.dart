88bb5c22024e2f2aa0892bb6d3eee4bc15f91da5
machk1auge <julian.knoll@tum.de>
2024-03-16 21:00:37 +0100

    departure_time added to URL.

--- lib/services/api_service_urlBuilder.dart
+++ lib/services/api_service_urlBuilder.dart
@@ -2,7 +2,7 @@ class URLBuilder {
   /// Builds the Uri for the Google Maps API
   /// Requires the API key and the coordinates of the origin and destination
   /// Coordinates are in the format [latitudeOrigin, longitudeQrigin, latitudeDestination, longitudeDestination]
-  static Uri buildUri(String apiKey, List<String> cords) {
+  static Uri buildUri(String apiKey, List<String> cords, DateTime departureTime) {
     return Uri(
         scheme: 'https',
         host: 'maps.googleapis.com',
@@ -11,6 +11,7 @@ class URLBuilder {
           'origin': '${cords[0]},${cords[1]}',
           'destination': '${cords[2]},${cords[3]}',
           'mode': 'transit',
+          'departure_time': (departureTime.millisecondsSinceEpoch ~/ 1000).toString(),
           'key': apiKey
         });
   }
