e6f233070af6387d0774e906f390c4b2d4f29907
machk1auge <julian.knoll@tum.de>
2024-03-16 20:10:50 +0100

    Fixed String Concatenation issue.

--- lib/services/api_service_urlBuilder.dart
+++ lib/services/api_service_urlBuilder.dart
@@ -1,19 +1,15 @@
 class URLBuilder {
-
-  /// Constructor of the URLBuilder
-  URLBuilder();
-
   /// Builds the Uri for the Google Maps API
   /// Requires the API key and the coordinates of the origin and destination
   /// Coordinates are in the format [latitudeOrigin, longitudeQrigin, latitudeDestination, longitudeDestination]
-  Uri buildUri(String apiKey, List<String> cords) {
+  static Uri buildUri(String apiKey, List<String> cords) {
     return Uri(
         scheme: 'https',
         host: 'maps.googleapis.com',
         path: '/maps/api/directions/json',
         queryParameters: {
-          'origin': '$cords[0],$cords[1]',
-          'destination': '$cords[2],$cords[3]',
+          'origin': '${cords[0]},${cords[1]}',
+          'destination': '${cords[2]},${cords[3]}',
           'mode': 'transit',
           'key': apiKey
         });
