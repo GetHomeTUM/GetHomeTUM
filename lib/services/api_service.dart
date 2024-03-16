378b9e89a7befa9d7d22943d1464de2362bb6ecc
Lennart Hesse <lennart.hesse@tum.de>
2024-03-16 18:30:01 +0100

    GH-8 api service überarbeitet

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,75 +1,87 @@
 import 'package:gethome/models/get_home_route.dart';
+import 'package:gethome/services/api_service_urlBuilder.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class GoogleAPI {
-  //Attribute
-  static String curr_latitude = "48.2655045";
-  static String curr_longitude = "11.6717073";
-  static String home_latitude = "48.1374";
-  static String home_longitude = "11.5754";
-  //interne Attribute
-  static int time = DateTime.now().millisecondsSinceEpoch ~/ 1000; //für Google API time als Sekunden nach 1970
-  static String uri = 'https://maps.googleapis.com/maps/api/directions/json?departure_time=$time&destination=$home_latitude,$home_longitude&origin=$curr_latitude,$curr_longitude&mode=transit&key=AIzaSyDQQTxn6Aak4y1HQcKzJ6svMoMLIvAV_rg';
-  static String output = ''; //hier steht dann der json-String -> stringToJsonMap(output) benutzen zum Decoden
 
-
-  //Returned ein Future, das gefragte liste zurückgibt -> "await getRoutes()" zum Abfragen benutzen
-  static Future<List<GetHomeRoute>> getRoutes() async {
+  /// getRoutes(String, List<String>) gibt ein Future zurück, dass eine Liste aus den drei nächsten Verbindungen ausgibt
+  /// Um das Output des Futures benutzen zu können, muss "await getRoutes()" benutzt werden!!
+  /// Falls eine Route fehlerhaft vorhanden ist, so wird diese auch aufgenommen
+  /// Falls für eine fehlerhafte Route keine Startzeit existiert, so wird der nächste Call für die gleiche Zeit durchgeführt
+  /// Parameter: apiKey,
+  /// cords: Liste aus Koordinaten von Start und Ziel im Format [latitudeOrigin, longitudeQrigin, latitudeDestination, longitudeDestination]
+  static Future<List<GetHomeRoute>> getRoutes(String apiKey, List<String> cords) async {
     List<GetHomeRoute> routes = [];
+    DateTime time = DateTime.now();
 
     //calls der nächsten drei Routen
     for (int i = 0; i < 3; i++) {
       //API call
-      await getResponse(fetchAlbum());
-      Map<String, dynamic> map = stringToJsonMap(output);
+      String output = await _getResponse(_fetchAlbum(apiKey, cords, time.add(const Duration(seconds: 60))));
+      // Umwandlung des Strings in eine Json-Map
+      Map<String, dynamic> map = _stringToJsonMap(output);
 
-      //Test für Error
-      if (output != '-Error-') {
-        //Erstellen einer neuen GetHomeRoute
-        GetHomeRoute simpleRoute = GetHomeRoute.fromJson(map);
-        routes.add(simpleRoute);
+      //Erstellen einer neuen GetHomeRoute
+      GetHomeRoute simpleRoute = GetHomeRoute.fromJson(map);
+      routes.add(simpleRoute);
+
+      // time auf den Zeitpunkt nach der ermittelten Route setzen
+      // Abfrage, ob Verbindung zulässig ist
+      if (map['routes'] != null && map['routes'][0]['legs'] != null) {
+        // tatsächliche Startzeit der Verbindung ermittlen (wurde nicht in GetHomeRoute ermittelt)
+        var start_time = map['routes'][0]['legs'][0]['departure_time']['value'];
         //früheste Startzeit der nächsten Verbindung setzen
-        int timeDiff = simpleRoute.departureTime.millisecondsSinceEpoch ~/ 1000 - time;
-        time += timeDiff +1;
+        int timeDiff = start_time - (time.millisecondsSinceEpoch ~/ 1000);
+        time = time.add(Duration(seconds: timeDiff+1));
       }
-      
     }
 
-    //Zeit Zurücksetzen für weitere calls
-    time = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-    
     return routes;
   }
 
-  //Hilfsmethode -> stringToJsonMap(ouput) aufrufen
-  static Map<String, dynamic> stringToJsonMap(String s) {
+
+
+  /// _stringToJsonMap(String) gibt eine Map aus einem Json-String aus
+  /// Parameter: s: String im Json-Format
+  static Map<String, dynamic> _stringToJsonMap(String s) {
     Map<String, dynamic> jsonMap = jsonDecode(s);
     return jsonMap;
   }
 
-  //Methoden, um von URL zu fetchen
-  static void refreshUri() {
-    uri = 'https://maps.googleapis.com/maps/api/directions/json?departure_time=$time&destination=$home_latitude,$home_longitude&origin=$curr_latitude,$curr_longitude&mode=transit&key=AIzaSyDQQTxn6Aak4y1HQcKzJ6svMoMLIvAV_rg';
+  /// Hilfsmethoden für API-call:
+  /// _fetchAlbum(String, String, String, String, DateTime) macht den API-call aus den übergebenen Parametern und gibt ein Future
+  /// zurück, welches die http.Response zurückgibt
+  /// Parameter:
+  /// apiKey,
+  /// cords: Liste aus Koordinaten im Format [latitudeOrigin, longitudeQrigin, latitudeDestination, longitudeDestination],
+  /// time: Startzeit der Verbindung, für die der API-call gemacht wird
+  /// 
+  /// _getResponse(Future<http.Response>) wertet eine http.Response aus und gibt ein Future zurück, welches den String der 
+  /// Response zurückgibt, falls die http.Response fehlerfrei ausgeführt werden konnte
+  /// Falls die http Request fehlerhaft war, wird ein Error geworfen
+  /// Parameter: future: Future, welches eine http Request macht und eine http.Response ausgibt
+  static Future<http.Response> _fetchAlbum(String apiKey, List<String> cords, DateTime time) {
+    return http.get(URLBuilder.buildUri(apiKey, cords, time));
   }
-  static Future<http.Response> fetchAlbum() {
-    refreshUri();
-    return http.get(Uri.parse(uri));
-  }
-  static Future<void> getResponse(Future<http.Response> future) async {
+  static Future<String> _getResponse(Future<http.Response> future) async {
     var response = await future;
     if (response.statusCode == 200) {
-      output = response.body;
+      return response.body;
     } else {
-      output = '-Error-';
+      throw Error();
     }
   }
 }
 
-
+/// main zum Testen
 void main() async {
-  List<GetHomeRoute> list = await GoogleAPI.getRoutes();
+  List<String> cords = ['48.2655045', '11.6717073', '48.1374', '11.5754'];
+  String apiKey = 'AIzaSyDQQTxn6Aak4y1HQcKzJ6svMoMLIvAV_rg';
+  List<GetHomeRoute> list = await GoogleAPI.getRoutes(apiKey, cords);
+  print(URLBuilder.buildUri(apiKey, cords, DateTime.now()).toString());
   print(list[0]);
   print(list[1]);
   print(list[2]);
+
 }
