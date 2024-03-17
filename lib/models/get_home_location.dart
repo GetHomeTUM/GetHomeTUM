bbf614c03ff48c8926f93300f9c55727df052af2
Janick Bunzel <janick@bunzel-online.de>
2024-03-17 20:00:49 +0100

    GH-13: GetHomeLocation: Added locationId and Documentation

--- lib/models/get_home_location.dart
+++ lib/models/get_home_location.dart
@@ -1,13 +1,32 @@
+/*
+  Documentation: GetHomeLocation
+
+  3 Variables:
+  - _id: String     locationId (ie, "home" or "work")
+  - _lat: double    latitude
+  - _lng: double    longitude
+
+  1 factory:
+  - GetHomeLocation.fromJson(Map<String, dynamic> json) - Converts a JSON object to a GetHomeLocation
+
+  2 Methods:
+  - toJson() - Converts a GetHomeLocation to a JSON-encodable map (default call by dart:convert.jsonEncode())
+  - getId() - Getter for the locationId
+  - toString() - Returns a string representation of the object
+*/
+
 class GetHomeLocation {
+  final String _id;
   final double _lat;
   final double _lng;
 
   // Creates a GetHomeLocation with the given latitude and longitude.
-  GetHomeLocation({required double lat, required double lng}) : _lat = lat, _lng = lng;
+  GetHomeLocation({required String id, required double lat, required double lng}) : _id = id, _lat = lat, _lng = lng;
 
-  // Creates a GetHomeLocation from a JSON object.
+  // Converts a JSON object to a GetHomeLocation
   factory GetHomeLocation.fromJson(Map<String, dynamic> json) {
     return GetHomeLocation(
+      id: json['id'],
       lat: json['lat'],
       lng: json['lng'],
     );
@@ -21,11 +40,15 @@ class GetHomeLocation {
     };
   }
 
+  // Getter for the locationId
+  String getId() => _id;
+
   //toString method
   @override
   String toString() {
     return 
-      '\n GetHomeLocationObject:'
+      '\n GetHomeLocation:'
+      '\n LocationId: $_id,'
       '\n Latitude: $_lat,'
       '\n Longitude: $_lng'
       '\n';
