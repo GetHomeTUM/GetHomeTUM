ea302b794ff45c2028be13bd9ccccbaf6f5c8875
Janick Bunzel <janick@bunzel-online.de>
2024-04-03 20:35:11 +0200

    GH-25: Removed GetHomeLocation.name

--- lib/models/get_home_location.dart
+++ lib/models/get_home_location.dart
@@ -1,7 +1,6 @@
 /// Documentation: GetHomeLocation
 /// 
-/// 3 Variables:
-/// - _name: String   locationName (ie, "home" or "work")
+/// 2 Variables:
 /// - _lat: double    latitude
 /// - _lng: double    longitude
 /// 
@@ -10,20 +9,17 @@
 /// 
 /// 2 Methods:
 /// - toJson() - Converts a GetHomeLocation to a JSON-encodable map (default call by dart:convert.jsonEncode())
-/// - getName() - Getter for the locationName
 /// - toString() - Returns a string representation of the object
 class GetHomeLocation {
-  final String _name;
   final double _lat;
   final double _lng;
 
   /// Creates a GetHomeLocation with the given latitude and longitude.
-  GetHomeLocation({required String name, required double lat, required double lng}) : _name = name.trim().toLowerCase(), _lat = lat, _lng = lng;
+  GetHomeLocation({required double lat, required double lng}) : _lat = lat, _lng = lng;
 
   /// Converts a JSON object to a GetHomeLocation
   factory GetHomeLocation.fromJson(Map<String, dynamic> json) {
     return GetHomeLocation(
-      name: json['name'].toString().trim().toLowerCase(),
       lat: json['lat'],
       lng: json['lng'],
     );
@@ -32,14 +28,12 @@ class GetHomeLocation {
   /// Converts a GetHomeLocation to a JSON-encodable map (default call by dart:convert.jsonEncode()).
   Map<String, dynamic> toJson() {
     return {
-      'name': _name,
       'lat': _lat,
       'lng': _lng,
     };
   }
 
   // Getters
-  String getName() => _name;
   double getLatitude() => _lat;
   double getLongitude() => _lng;
 
@@ -48,7 +42,6 @@ class GetHomeLocation {
   String toString() {
     return 
       '\n GetHomeLocation:'
-      '\n LocationName: $_name,'
       '\n Latitude: $_lat,'
       '\n Longitude: $_lng'
       '\n';
