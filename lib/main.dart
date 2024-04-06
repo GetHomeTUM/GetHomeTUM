2893bd4ad829d4f8f3ad1e70c999ece2e2c04f2e
Lennart Hesse <lennart.hesse@tum.de>
2024-04-06 21:36:27 +0200

    GH-24: letzter Stand zum workmanager

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:gethome/services/local_storage_service.dart';
 import 'package:gethome/views/home_screen.dart';
+
+import 'package:workmanager/workmanager.dart';
+//import 'package:gethome/services/update_widget_service.dart';
+
+// ANMERKUNG: Simulation in Xcode starten, danach Debug > Simulate Background Fetch
+// um background fetch zu simulieren und die callbackDispatcher Methode auszuführen
+
+
+@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+void callbackDispatcher() async {
+  Workmanager().executeTask((task, inputData) async {
+    print("fetching...");
+    print(await LocalStorageService.loadLocation('Home')); // throws error
+    //use following *test* method for a simple widget update without parameters
+    //UpdateWidgetService.updateBackgroundWidget();
+    return Future.value(true);
+  });
+}
 
 /// Main for testing the application. Insert your own key for the directions API here.
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(
+    callbackDispatcher, // The top level function, aka callbackDispatcher
+    isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+  );
+  
+  
+  // starting application
   const String apiKey = '';
   runApp(const GetHomeApp(apiKey));
 }
--- lib/services/update_widget_service.dart
+++ lib/services/update_widget_service.dart
@@ -1,9 +1,89 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:gethome/services/current_location_service.dart';
 import 'package:gethome/views/route_widget.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:gethome/models/get_home_route.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:gethome/models/get_home_location.dart';
+import 'package:gethome/services/local_storage_service.dart';
+import 'package:gethome/services/api_service.dart';
 
 class UpdateWidgetService {
+  
+  // test method to simply update widget without any parameters (nicht getestet aber sollte funktionieren)
+  static void updateBackgroundWidget() async {
+      // list where the next three GetHomeRoutes are saved in
+  List<GetHomeRoute>? _nextRoutes;
+  // LatLng object that stores the home location once it's available
+  LatLng? _homePosition;
+  // API key
+  const String _apiKey = 'AIzaSyAUz_PlZ-wSsnAqEHhOwRX19Q2O-gMEVZw';
+
+
+  print('started');
+  // update home position
+  GetHomeLocation? location = await LocalStorageService.loadLocation('Home');
+  if (location != null) {
+    _homePosition = LatLng(location.getLatitude(), location.getLongitude());
+  }
+  //_homePosition = LatLng(48.26388, 11.6688246);
+
+  print('getting home location finished');
+
+  // update current location
+  /*Position? position;
+    await LocationService.getCurrentLocation()
+        .then((value) => position = value)
+        .catchError((error) {
+      position = null;
+      return Future.value(position);
+    });
+    */
+    var position = LatLng(48.15003, 11.54555);
+
+    print('getting cords finished');
+
+    // making the API call using both the device's location and the home location
+    if (position != null && _homePosition != null) {
+      // list of coordinates
+      List<String> cords = [
+        position!.latitude.toString(),
+        position!.longitude.toString(),
+        _homePosition!.latitude.toString(),
+        _homePosition!.longitude.toString()
+      ];
+      List<GetHomeRoute> list = List.empty();
+      try {
+        // API call
+        list = await GoogleAPI.getRoutes(_apiKey, cords);
+      } catch (error) {
+        print('no connection found (background error)');
+      }
+      // setting the value of the _nextRoutes list
+      _nextRoutes = list;
+    }
+    print('getting routes finished');
+
+
+    // render image
+    var path = await HomeWidget.renderFlutterWidget(
+        _nextRoutes != null && _nextRoutes!.isNotEmpty
+            ? // either the RouteWidget of a DefaultImage is rendered
+            RouteWidget(nextRoutes: _nextRoutes!)
+            : const DefaultImage(),
+        // data for storing and rendering the image
+        key: 'filename',
+        logicalSize: Size(430.0, 230.0),
+      ) as String;
+
+    HomeWidget.updateWidget(
+        iOSName: 'GetHomeIos', androidName: 'GetHomeWidgetProvider');
+    print('ended');
+  }
+
   /// Method for updating the home_widget. It needs a global key for the size and an optional
   /// list of the next GetHomeRoutes. The method renders the image of the widget first and then
   /// updated it.
