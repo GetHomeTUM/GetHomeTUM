73bd8e20c56770b9fda743fe0bb9ad71ffc5c7ce
Lennart Hesse <lennart.hesse@tum.de>
2024-04-12 12:53:43 +0200

    GH-24: AppDelegate Änderungen

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,6 +12,9 @@ import workmanager
     // In AppDelegate.application method
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
     WorkmanagerPlugin.registerTask(withIdentifier: "task-identifier")
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+            GeneratedPluginRegistrant.register(with: registry)
+        }
     GMSServices.provideAPIKey("AIzaSyCF7hSAusM2Jak6H62GVVKxr-QP_Z3rJ_g")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
