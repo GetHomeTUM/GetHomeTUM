eff27e342d9888129f172d3d8f21a4924ec75f38
Janick Bunzel <janick@bunzel-online.de>
2024-04-15 17:25:53 +0200

    GH-38: Added prefix to storage keys for different data types

--- lib/services/local_storage_service.dart
+++ lib/services/local_storage_service.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:gethome/models/get_home_location.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Enum to diffrentiate between the different types of stored valuess
+enum StorageKeyTypes{
+  location,
+  userSetting,
+}
 
 /// Documentation: LocalStorageService
 /// 
@@ -18,21 +23,27 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// - removeBoolean(String key) - Future<void>: Removes a boolean for the given key.
 class LocalStorageService{
   /// Instance of shared preferences
-  static SharedPreferences? _preferences;
-  
-  /// private constructor
-  LocalStorageService._();
+  static SharedPreferences? _storageInstance;
   
   /// Private method for checking and initializing the shared preferences instance
-  static Future<void> _checkPreferencesInitialization() async {
+  static Future<void> _checkStorageInstance() async {
     /// if not already initialized, initialize the shared preferences instance
-    _preferences ??= await SharedPreferences.getInstance();
+    _storageInstance ??= await SharedPreferences.getInstance();
   }
 
+  /// Method for generating the key for a given type from the StorageKeyTypes
+  static String _computeKey(String key, StorageKeyTypes type){
+    String prefix = switch(type){
+      StorageKeyTypes.location => "location",
+      StorageKeyTypes.userSetting => "userSetting",
+    };
+
+    return "KeyType-${prefix}_${key.trim().toLowerCase()}";
+  }
   
   /// Method for saving a location by the given key -> overwrites the location if the key already exists and returns true if successful
   static Future<bool> setLocation(String key, GetHomeLocation location) async {
-    await _checkPreferencesInitialization();
+    await _checkStorageInstance();
     
     /// Convert the GetHomeLocation object to a JSON String
     String locationJson;
@@ -43,23 +54,23 @@ class LocalStorageService{
       return false;
     }
 
-    /// Use _preferences to save the location as the JSON String
+    /// Use _storageInstance to save the location as the JSON String
     try {
-      return await _preferences!.setString(key.trim().toLowerCase(), locationJson).then((value) => value);
+      return await _storageInstance!.setString(_computeKey(key, StorageKeyTypes.location), locationJson);
     } catch (e) {
-      debugPrint("Error at _preferences!.setString(key, locationJson): $e");
+      debugPrint("Error at _storageInstance!.setString(key, locationJson): $e");
       return false;
     }
   }
 
   /// Method for loading a location by given key -> returns null if the key does not exist
   static Future<GetHomeLocation?> getLocation(String key) async {
-    await _checkPreferencesInitialization();
+    await _checkStorageInstance();
 
     /// Check if the location with the given key exists
-    if(_preferences!.containsKey(key.trim().toLowerCase())){
-      /// Load the location as a JSON String from _preferences
-      String? locationJson = _preferences!.getString(key.trim().toLowerCase());
+    if(_storageInstance!.containsKey(_computeKey(key, StorageKeyTypes.location))){
+      /// Load the location as a JSON String from _storageInstance
+      String? locationJson = _storageInstance!.getString(_computeKey(key, StorageKeyTypes.location));
       
       /// Convert the JSON String to a GetHomeLocation object
       return GetHomeLocation.fromJson(jsonDecode(locationJson!));
@@ -70,18 +81,18 @@ class LocalStorageService{
 
   /// Method for checking if a location with the given key exists
   static Future<bool> checkLocation(String key) async {
-    await _checkPreferencesInitialization();
+    await _checkStorageInstance();
 
     // Check if the location with the given key exists
-    return _preferences!.containsKey(key.trim().toLowerCase());
+    return _storageInstance!.containsKey(_computeKey(key, StorageKeyTypes.location));
   }
 
   /// Method for removing the location with given key
   static Future<void> removeLocation(String key) async {
-    await _checkPreferencesInitialization();
+    await _checkStorageInstance();
 
     // Delete the location with given key
-    await _preferences!.remove(key.trim().toLowerCase());
+    await _storageInstance!.remove(_computeKey(key, StorageKeyTypes.location));
   }
 
 
